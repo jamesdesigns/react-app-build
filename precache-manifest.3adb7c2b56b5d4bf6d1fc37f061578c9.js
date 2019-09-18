37ee37ca8a6d4b97b06c3a2dc8d360d47f8048a3
James Hooper <jameshooperdesigns@gmail.com>
2019-09-18 14:26:43 -0600

    Updates

---
+++ precache-manifest.3adb7c2b56b5d4bf6d1fc37f061578c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc37b9e0080ccd2ad6bae773b7f6a3b6",
+    "url": "/react-app-build/index.html"
+  },
+  {
+    "revision": "6e73e4b2e295d015a646",
+    "url": "/react-app-build/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "82d3a10f304e5e550be5",
+    "url": "/react-app-build/static/js/2.95ab734c.chunk.js"
+  },
+  {
+    "revision": "6e73e4b2e295d015a646",
+    "url": "/react-app-build/static/js/main.d481950e.chunk.js"
+  },
+  {
+    "revision": "fd0ee831306baca19504",
+    "url": "/react-app-build/static/js/runtime~main.afeb8c51.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-app-build/static/media/logo.5d5d9eef.svg"
+  }
+]);
