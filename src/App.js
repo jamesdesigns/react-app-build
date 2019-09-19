9306bb3ea20a9bda5ef1f1de741184a02d44ef6e
James Hooper <jameshooperdesigns@gmail.com>
2019-09-19 17:30:17 -0600

    Add changes to the deletCharHandler in the App.js on line 17-21

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import Validation from './Validation/Validation';
+import Char from './Char/Char';
+import { isThisExpression } from '@babel/types';
 
 class App extends Component {
 state = {
@@ -12,7 +14,21 @@ inputChangedHandler = (event) => {
   this.setState({userInput: event.target.value});
 }
 
+deleteCharHandler = (index) => {
+  const text = this.state.userInput.split('');
+  text.splice(index, 1);
+  const updatedText = text.join('');
+  this.setState({userInput: updatedText});
+}
+
 render() {
+  const charList = this.state.userInput.split('').map((ch, index) => {
+    return <Char 
+    character={ch} 
+    key={index} 
+    clicked={() => this.deleteCharHandler(index)} />;
+  });
+
   return (
     <div className="App">
 
@@ -24,6 +40,7 @@ render() {
      value={this.state.userInput} />
      <p>{this.state.userInput}</p>
      <Validation inputLength={this.state.userInput.length} />
+     {charList}
     </div>
   );
 }
